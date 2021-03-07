ede63c8859999377f2d4b890fb6f835cd2878dea
satya <ksnraju82@gmail.com>
2021-03-08 06:24:55 +1000

    Initial Commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+
+import {  throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private projectURL = "http://localhost:3000/projects";
+  private environmentURL = "http://localhost:3000/environments";
+  private releaseURL = "http://localhost:3000/releases";
+  private deploymentsURL = "http://localhost:3000/deployments";
+
+  constructor(private httpClient: HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
+  public getProjects(){
+    return this.httpClient.get(this.projectURL).pipe(catchError(this.handleError));
+  }
+  public getEnvironments(){
+    return this.httpClient.get(this.environmentURL).pipe(catchError(this.handleError));
+  }
+  public getReleases(){
+    return this.httpClient.get(this.releaseURL).pipe(catchError(this.handleError));
+  }
+  public getDeployments(){
+    return this.httpClient.get(this.deploymentsURL).pipe(catchError(this.handleError));
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,70 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  projects = [];
+  environments = [];
+  releases = [];
+  deployments = [];
+  constructor(private apiService: ApiService) {}
   title = 'Deployments';
+
+  ngOnInit() {
+    Promise.all([this.getProjects(), this.getEnvironments(), this.getReleases(), this.getDeployments()]).then(() => {
+      if (this.releases && this.deployments && this.environments && this.projects) {
+        
+      }
+    });
+  }
+  
+  getProjects() {
+    const promise = new Promise((resolve, reject) =>{
+      this.apiService.getProjects().subscribe((data: any[])=>{			
+        this.projects = data;  
+        resolve(true);
+      }, error => {
+        console.log(error);
+        reject(false);
+      }); 
+    });
+    return promise;
+  }
+
+  getEnvironments() {
+    const promise = new Promise((resolve, reject) =>{
+      this.apiService.getEnvironments().subscribe((data: any[])=>{ 			
+        this.environments = data;  
+        resolve(true);
+      }, error => {
+        console.log(error);
+        reject(false);
+      });  
+    });
+    return promise;
+  }
+
+  getReleases() {
+    const promise = new Promise((resolve, reject) =>{
+      this.apiService.getReleases().subscribe((data: any[])=>{  			
+        this.releases = data;  
+        resolve(true);
+      }, error => {
+        console.log(error);
+        reject(false);
+      });  
+     });
+    return promise;
+  }
+
+  getDeployments() {
+    const promise = new Promise((resolve, reject) =>{ 
+      this.apiService.getDeployments().subscribe((data: any[])=>{  			
+        this.deployments = data;  
+        resolve(true)
+      }, error => {
+        console.log(error);
+        reject(false);
+      });  
+    });
+    return promise;
+  }
 }
